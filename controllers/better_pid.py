6b5cf5cb25d4fe5af2a22f535c396de434216825
Tudor Oancea <oancea.tudor@icloud.com>
2025-11-11 09:21:05 +0100

    formatting

--- controllers/better_pid.py
+++ controllers/better_pid.py
@@ -2,19 +2,25 @@ from icecream import ic
 from . import BaseController
 import numpy as np
 
+
 class Controller(BaseController):
-  def __init__(self,):
+  def __init__(
+    self,
+  ):
     self.p = 0.195
     self.i = 0.100
     self.d = -0.053
     self.error_integral = 0
     self.prev_error = 0
-    self.prev_lataccel : float|None = None
+    self.prev_lataccel: float | None = None
     self.lam = 0.75
 
   def update(self, target_lataccel, current_lataccel, state, future_plan):
-    if self.prev_lataccel is not None: current_lataccel = current_lataccel * self.lam + self.prev_lataccel * (1-self.lam)
-    error = (target_lataccel - current_lataccel)
+    if self.prev_lataccel is not None:
+      current_lataccel = current_lataccel * self.lam + self.prev_lataccel * (
+        1 - self.lam
+      )
+    error = target_lataccel - current_lataccel
     self.error_integral += error
     error_diff = error - self.prev_error
     self.prev_error = error
